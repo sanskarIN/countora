e7755d89aa7d96b4d3485707cd8fc516bc3831cb
Sanskar <sanskarin@outlook.in>
2026-08-19 09:05:00 +0530

    feat: add application links

---
+++ lib/src/core/app_links.dart
@@ -0,0 +1,9 @@
+abstract final class AppLinks {
+  static const github = 'https://github.com/sanskarIN';
+  static const repository = 'https://github.com/sanskarIN/countora';
+  static const buyMeACoffee = 'https://buymeacoffee.com/sanskarIN';
+
+  static const businessEmail = 'sanskarin@outlook.in';
+  static const secondaryBusinessEmail = 'sanskarin.business@gmail.com';
+  static const supportEmail = 'supportramsandesh@gmail.com';
+}
