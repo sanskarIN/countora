f8899f40e16d89e59a6f5cdecfd50f4d83ce14d6
Sanskar <sanskarin@outlook.in>
2026-08-19 10:04:46 +0530

    chore: centralize Countora release metadata

---
+++ lib/src/core/app_metadata.dart
@@ -0,0 +1,8 @@
+abstract final class AppMetadata {
+  static const name = 'Countora';
+  static const version = '0.2.0';
+  static const buildNumber = 2;
+  static const license = 'MIT License';
+  static const credit = 'Made by the Sanskar';
+  static const storageModel = 'Local-first';
+}
